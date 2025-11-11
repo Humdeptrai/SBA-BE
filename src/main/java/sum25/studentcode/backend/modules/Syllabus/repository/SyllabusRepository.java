d711d7c8b38fc4d2f641c3d17359878f8305e1cf
hoangtrungvo <135581559+hoangtrungvo@users.noreply.github.com>
2025-11-11 10:22:35 +0700

    up

--- src/main/java/sum25/studentcode/backend/modules/Syllabus/repository/SyllabusRepository.java
+++ src/main/java/sum25/studentcode/backend/modules/Syllabus/repository/SyllabusRepository.java
@@ -1,6 +1,7 @@
 package sum25.studentcode.backend.modules.Syllabus.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import sum25.studentcode.backend.modules.Syllabus.entity.Syllabus;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 @Repository
 public interface SyllabusRepository extends JpaRepository<Syllabus, Long> {
+    @Query("SELECT s FROM Syllabus s WHERE s.createdBy_UserId = ?1")
     List<Syllabus> findByCreatedBy_UserId(Long createdBy_UserId);
 }
